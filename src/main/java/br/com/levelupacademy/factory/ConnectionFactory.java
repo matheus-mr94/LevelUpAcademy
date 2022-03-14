4be19a6707e1bbe2ddf6ac1763506fe1769cf54e
Matheus Reis <mmr94@live.com>
2022-03-14 09:55:21 -0300

    criando pool de conexões

--- src/main/java/br/com/levelupacademy/factory/ConnectionFactory.java
+++ src/main/java/br/com/levelupacademy/factory/ConnectionFactory.java
@@ -1,14 +1,26 @@
 package br.com.levelupacademy.factory;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
 
+    private DataSource dataSource;
+
+    public ConnectionFactory() {
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource
+                .setJdbcUrl("jdbc:mysql://localhost/LevelUpAcademy?useTimezone=true&server=UTC");
+        comboPooledDataSource.setUser("root");
+        comboPooledDataSource.setPassword("Root@2102");
+
+        this.dataSource = comboPooledDataSource;
+    }
+
     public Connection recoverConnection() throws SQLException {
-     return DriverManager
-                .getConnection("jdbc:mysql://localhost/LevelUpAcademy?useTimezone=true&server=UTC",
-                "root", "Root@2102");
+     return this.dataSource.getConnection();
     }
 }
