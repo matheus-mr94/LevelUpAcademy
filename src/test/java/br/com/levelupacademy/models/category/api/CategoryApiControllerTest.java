125afe927983d2f2b6f6c86f7ad42e7d199645da
Matheus Reis <mmr94@live.com>
2022-05-02 16:48:42 -0300

    colocado anotação de profile na classe de teste da controller da api

--- src/test/java/br/com/levelupacademy/models/category/api/CategoryApiControllerTest.java
+++ src/test/java/br/com/levelupacademy/models/category/api/CategoryApiControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -32,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestPropertySource(properties = {"DB_NAME=LevelUpAcademy_test", "spring.jpa.hibernate.ddlAuto:create-drop"})
 @AutoConfigureMockMvc
 @AutoConfigureTestEntityManager
+@ActiveProfiles("test")
 @Transactional
 class CategoryApiControllerTest {
 
